ce48cf162447cb442e36b0fef4335ef7e5a18450
YunShangzhou <912453237@qq.com>
2022-05-08 23:55:58 +0800

    上推一个测试git actions

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(fas);
+
+
 export { default as Button } from "./components/Button/";
 export { default as Menu } from "./components/Menu/";
 export { default as Icon } from "./components/Icon/";
@@ -8,5 +11,4 @@ export { default as Input } from "./components/Input/";
 export { default as Upload } from "./components/Upload/";
 export { default as Progress } from "./components/Progress/";
 export { default as Transition } from "./components/Transition/";
-library.add(fas);
 
